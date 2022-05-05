9fe7f0d52a1588e2e5b3d5d783e193df32dec170
celso patiri <celso@pop-os.localdomain>
2022-05-04 21:50:22 -0300

    feat: auth router signin and signup structure

--- server/src/routes/auth/auth.router.ts
+++ server/src/routes/auth/auth.router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { logUserIn } from "../../controllers/auth/signin.controller";
 import { registerUser } from "../../controllers/auth/signup.controller";
 
 const router = Router();
 
 router.post("/signup", registerUser);
+router.post("/signin", logUserIn);
 
 export default router;
