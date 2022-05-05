fa44e157416b5242a0cb6a01e742160fedcc5088
celso patiri <celso@pop-os.localdomain>
2022-05-05 14:58:49 -0300

    feat: findSession service

--- server/src/services/sessions/sessions.services.ts
+++ server/src/services/sessions/sessions.services.ts
@@ -2,6 +2,17 @@ import { Types } from "mongoose";
 import { v4 as uuid } from "uuid";
 import { Session } from "../../models/sessions/session.model";
 
+export const findSession = async (user_id: Types.ObjectId, token: string) => {
+  try {
+    return await Session.findOne({
+      user_id,
+      token,
+    });
+  } catch (err) {
+    throw err;
+  }
+};
+
 const cleanExistingSession = async (user_id: Types.ObjectId) => {
   try {
     await Session.deleteOne({ user_id });
