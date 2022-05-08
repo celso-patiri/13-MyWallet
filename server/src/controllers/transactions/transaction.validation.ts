02fa357ce0630a4e3577ed96ec7531fd0ae99709
celso patiri <celso@pop-os.localdomain>
2022-05-07 21:54:33 -0300

    fix: regex pattern exact match

--- server/src/controllers/transactions/transaction.validation.ts
+++ server/src/controllers/transactions/transaction.validation.ts
@@ -3,27 +3,31 @@ import Joi from "joi";
 import { TransactionRequestBody } from "../../global/request.types";
 
 const transactionSchema = Joi.object()
-  .keys({
-    description: Joi.string().min(3).required().messages({
-      "string.min": "Description must be at least 3 characters long",
-      "any.required": "Description is required",
-    }),
-    income: Joi.boolean().required().messages({
-      "any.required": "Income boolean is required",
-    }),
-    date: Joi.date().required().messages({
-      "any.required": "Date is required",
-    }),
-  })
-  .unknown();
+    .keys({
+        value: Joi.string()
+            .pattern(/^([0-9]{0,5},[0-9]{1,2})|([0-9]*)$/)
+            .required()
+            .messages({ "string.pattern": "Value must be a valid number" }),
+        description: Joi.string().min(3).required().messages({
+            "string.min": "Description must be at least 3 characters long",
+            "any.required": "Description is required",
+        }),
+        isIncome: Joi.boolean().required().messages({
+            "any.required": "isIncome boolean is required",
+        }),
+        date: Joi.date().required().messages({
+            "any.required": "Date is required",
+        }),
+    })
+    .unknown();
 
 export const validateTransaction = async (
-  req: TransactionRequestBody,
-  res: Response,
-  next: NextFunction
+    req: TransactionRequestBody,
+    res: Response,
+    next: NextFunction
 ) => {
-  const { error } = transactionSchema.validate(req.body);
-  if (error) return res.status(422).send(error.details[0]);
+    const { schemaError } = transactionSchema.validate(req.body);
+    if (schemaError) return res.status(422).send(schemaError.details[0]);
 
-  next();
+    next();
 };
