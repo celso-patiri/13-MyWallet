ae4ae42028aa87cd9d1c94e12ddf32745a8e806e
celso patiri <celso@pop-os.localdomain>
2022-05-07 21:55:27 -0300

    fix: ITransaction type "isIncome" consistency

--- server/src/models/transactions/transaction.model.ts
+++ server/src/models/transactions/transaction.model.ts
@@ -1,17 +1,17 @@
 import { model, Schema, Types } from "mongoose";
 
 export interface ITransaction {
-  description: String;
-  income: Boolean;
-  date: Date;
-  user_id: Types.ObjectId;
+    description: String;
+    isIncome: Boolean;
+    date: Date;
+    user_id: Types.ObjectId;
 }
 
 const transactionSchema = new Schema<ITransaction>({
-  description: { type: String, required: true },
-  income: { type: Boolean, required: true },
-  date: { type: Date, required: true },
-  user_id: { type: Schema.Types.ObjectId, required: true },
+    description: { type: String, required: true },
+    isIncome: { type: Boolean, required: true },
+    date: { type: Date, required: true },
+    user_id: { type: Schema.Types.ObjectId, required: true },
 });
 
 export const Transaction = model<ITransaction>("Transaction", transactionSchema);
