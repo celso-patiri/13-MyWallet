c825c0cf71837173108e422df9afe12a7bf49317
celso patiri <celso@pop-os.localdomain>
2022-05-09 14:23:49 -0300

    chore: heroku deploy fix

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,11 +4,11 @@ import app from "./app";
 const PORT = process.env.PORT;
 const MONGO_BASE = process.env.MONGO_URI;
 const DB_NAME = process.env.DB_NAME;
-const MONGO_URI = `${MONGO_BASE}/${DB_NAME}`;
+const MONGO_URI = `${MONGO_BASE}`;
 
 mongoose
-  .connect(MONGO_URI)
-  .then(() => console.log(`Connected with mongoose on ${MONGO_URI}`))
-  .catch(console.dir);
+    .connect(MONGO_URI)
+    .then(() => console.log(`Connected with mongoose on ${MONGO_URI}`))
+    .catch(console.dir);
 
 app.listen(PORT, () => console.log(`Listening on ${PORT} `));
